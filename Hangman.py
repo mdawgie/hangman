27f616bcfd5dd288d24056fa6270ab38d6a46adf
mdawgie <54043444+mdawgie@users.noreply.github.com>
2019-08-16 14:53:51 -0600

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -6,16 +6,34 @@ def main():
     word_list = ["Hello","Keyboard","Rock","Paper","Scissors","Mouse", "Moniter","Computer","Python"]
     randomchoice = random.choice(word_list)
     lenth = len(randomchoice)
-    print("The answer is a",lenth,"letter word. You have",lenth,"lives remaining")
-    guess = input("Guess a letter")
-    for letter in randomchoice:
-        if guess == letter:
+    print("The answer is a",lenth,"letter word.")
+    lives = lenth
+    guessed_letters = []
+    while lives>0:
+        
+        print("You have",lenth,"lives remaining")
+        guess = input("Guess a letter")
+        guessed_letters.append(guess)
+        if checkWin(guessed_letters, randomchoice):
+            print("Correct,",guess)
+        else:
+            print("Incorrect")
+            lives = lives - 1
+                        
+    if (lives == 0):
+        print("You Lose")
+    
             
-        print("Correct")
-    
+                 
+
 def checkWin(guessed_letters, selected_word):
-    pass
-    
+    flag = False
+    for letter in selected_word:
+        if guessed_letters[-1] == letter:
+            flag = True
+     
+    return flag
+      
 
 if __name__ == "__main__":
     main()
